fa5ea48aefcf4a1c275cdacf7ab70ba8a90f1225
Corey Mckeel <coreyt.mckeel@gmail.com>
2021-12-14 23:22:35 -0500

    belt exam prep demo

---
+++ Week3/Session1/static/script.js
@@ -0,0 +1,12 @@
+console.log("Connected to js")
+
+function changeColorLime(element) {
+    element.style.color = "lime";
+}
+function changeColorBack(element) {
+    element.style.color = "white";
+}
+
+function hideMid() {
+    document.querySelector("#mid").style.display = "none";
+}
